9527389adc3e64807b0f37085b7c75a80187e69b
DESKTOP-FKODTS1\Admin <phamviethuy1210@gmail.com>
2024-10-08 02:29:47 +0700

    Init for EX5

---
+++ STM32Project/Core/Inc/software_timer.h
@@ -0,0 +1,15 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Oct 8, 2024
+ *      Author: Admin
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+#include "main.h"
+
+void display7SEG(int num);
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
